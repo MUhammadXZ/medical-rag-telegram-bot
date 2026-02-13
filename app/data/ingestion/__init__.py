7fa06a3b4cc3e88c12710c3f2eef4b5a894755ef
MUhammadXZ <muhammadfakhr73@gmail.com>
2026-02-13 17:22:01 +0200

    Add FAISS index reload helper and index-size tests

--- app/data/ingestion/__init__.py
+++ app/data/ingestion/__init__.py
@@ -1,6 +1,13 @@
 """Data ingestion utilities for loading and chunking guideline documents."""
 
-from .pipeline import ingest_guidelines
+from .pipeline import build_faiss_index, ingest_guidelines, load_faiss_index, rebuild_index
 from .models import DocumentChunk, IngestionConfig
 
-__all__ = ["ingest_guidelines", "DocumentChunk", "IngestionConfig"]
+__all__ = [
+    "ingest_guidelines",
+    "build_faiss_index",
+    "rebuild_index",
+    "load_faiss_index",
+    "DocumentChunk",
+    "IngestionConfig",
+]
